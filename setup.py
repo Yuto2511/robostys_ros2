5c93751b0fd3a92de917c4fa638a56c487b02392
Yuto2511 <efmgh2511@outlook.jp>
2023-01-07 14:59:22 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'robosys_ros2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yuto',
+    maintainer_email='efmgh2511@outlook.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
